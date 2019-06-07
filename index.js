9dda71ae36d1be7f982a5c9f68919efd3d9c5667
nadeeraka <nimanthanadeeraka@outlook.com>
2019-06-07 20:05:14 +0530

    now speeking

--- index.js
+++ index.js
@@ -12,8 +12,22 @@ recognition.onstart = () => {
 
 recognition.onresult = event => {
   console.log(event);
+  const current = event.resultIndex;
+  const transcript = event.results[current][0].transcript;
+  content.textContent = transcript;
+  Cortana(transcript);
+  console.log(transcript);
 };
 
 talk.addEventListener("click", () => {
   recognition.start();
 });
+
+const Cortana = data => {
+  const speech = new SpeechSynthesisUtterance();
+  speech.text = data;
+  speech.rate = 1;
+  speech.pitch = 1;
+  speech.volume = 1;
+  window.speechSynthesis.speak(speech);
+};
