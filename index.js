bdf46b48c934374ca1a764c9330846ef30e6ce0e
nadeeraka <nimanthanadeeraka@outlook.com>
2019-06-07 19:24:07 +0530

    add basic content

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-alert("oj");
+const talk = document.querySelector(".talk");
+const content = document.querySelector(".content");
+
+const speechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new speechRecognition();
+
+recognition.onstart = () => {
+  console.log("voce recognition is started you can speech");
+};
+
+recognition.onresult = event => {
+  console.log(event);
+};
+
+talk.addEventListener("click", () => {
+  recognition.start();
+});
